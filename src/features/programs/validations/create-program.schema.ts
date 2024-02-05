4af351b6cb827047b1dbeaa5d8ef88831f3be906
oleksiizdDevima <oleksii@devima.solutions>
2024-02-05 19:01:47 +0200

    fix: removed programs

--- src/features/programs/validations/create-program.schema.ts
+++
@@ -1,7 +0,0 @@
-import * as Joi from 'joi';
-
-import { CreateProgramDto } from '../dto';
-
-export const createProgramSchema = Joi.object<CreateProgramDto>({
-  name: Joi.string().trim().required().max(255),
-});
--- src/features/programs/validations/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './create-program.schema';
-export * from './update-program.schema';
